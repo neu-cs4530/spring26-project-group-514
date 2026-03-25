08de397df81efb18538f1594e552b4289444c5d0
khvitri <khvitri@gmail.com>
2026-03-25 12:36:35 -0400

    Implemented endpoint for user to remove a friend

--- server/src/app.ts
+++ server/src/app.ts
@@ -69,7 +69,8 @@ app.use(
         .get("/list/:username", friend.getList)
         .get("/requests/:username", friend.getRequests)
         .post("/request", friend.postRequest)
-        .post("/respond", friend.postRespond),
+        .post("/respond", friend.postRespond)
+        .post("/remove", friend.postRemove),
     )
     .use("/dm", express.Router().get("/list/:username", dm.getList).get("/:id", dm.getById)),
 );
