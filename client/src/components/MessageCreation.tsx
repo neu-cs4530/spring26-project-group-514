5bbe23f0a544e0fad1428f8633ce24976b6557bb
ljchandler21 <loganjchandler@gmail.com>
2026-04-10 16:54:57 -0400

    EMOJIS!!! V1
    I wanna fix some stylistics but the function is there.

--- client/src/components/MessageCreation.tsx
+++ client/src/components/MessageCreation.tsx
@@ -1,5 +1,6 @@
 import "./MessageCreation.css";
 import { type SubmitEvent, type KeyboardEvent, useState } from "react";
+import EmojiPicker from "./EmojiPicker.tsx";
 
 interface MessageCreationProps {
   handleMessageCreation: (text: string) => void;
@@ -10,7 +11,7 @@ export default function MessageCreation({ handleMessageCreation }: MessageCreati
 
   function handleKeyDown(e: KeyboardEvent<HTMLTextAreaElement>) {
     if (e.code === "Enter" && !e.shiftKey) {
-      e.preventDefault(); // Don't edit text
+      e.preventDefault();
       handleMessageCreation(text);
       setText("");
     }
@@ -24,12 +25,13 @@ export default function MessageCreation({ handleMessageCreation }: MessageCreati
 
   return (
     <form data-testid="message-creation-form" className="messageCreation" onSubmit={handleSubmit}>
+      <EmojiPicker onSelect={(emoji) => setText((prev) => prev + emoji)} />
       <textarea
         placeholder="Send a message to chat"
         value={text}
         onKeyDown={handleKeyDown}
         onChange={(e) => setText(e.target.value)}
-      ></textarea>
+      />
       <button className="visuallyHidden">Submit</button>
     </form>
   );
